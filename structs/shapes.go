48709e7c4b6ddad1b53d008be0a9db56e98ab710
Brey Rivera <breyr2021@gmail.com>
2024-05-10 13:06:06 -0400

    structs and interfaces

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,14 +1,33 @@
 package structs
 
+import "math"
+
+// Cannot overload functions in Go!
+// func (recieverName RecieverType) MethodName (args)
+// convention to make the recieverName the first letter of its type
+
+// interface resolution is implicit, passing a type that matches what the interface is asking for it will compile
+type Shape interface {
+	Area() float64
+}
+
 type Reactangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rect Reactangle) float64 {
-	return 2 * (rect.Width + rect.Height)
+func (r Reactangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
-func Area(rect Reactangle) float64 {
-	return rect.Width * rect.Height
+func (r Reactangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rect := Reactangle{10.0, 10.0}
-	got := Perimeter(rect)
+	got := rect.Perimeter()
 	want := 40.0
 
 	//  %.2f - display two decimal place float64
@@ -14,10 +14,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Reactangle{12.0, 8.0}
-	got := Area(rect)
-	want := 96.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Reactangle{12.0, 8.0}
+		want := 96.0
+		checkArea(t, rect, want)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
 }
