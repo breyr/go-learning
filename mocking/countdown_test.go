e1c860171fe1e6e3359214cb878865ef68291416
Brey Rivera <breyr2021@gmail.com>
2024-05-12 14:11:56 -0400

    mocking

---
+++ mocking/countdown_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+// mock what sleeping is doing so that we aren't dependent on time
+
+const write = "write"
+const sleep = "sleep"
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+// keep track of order of operations
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestCountdown(t *testing.T) {
+	// useful for testing io.Writer interface implementations
+	// in main() data will be sent out to os.Stdout
+	// in test we will write data to bytes.Buffer so the tests can capture the output
+
+	// SpyCountdownOperations implements both io.Writer and Sleeper
+	spySleepPrinter := &SpyCountdownOperations{}
+	Countdown(spySleepPrinter, spySleepPrinter)
+
+	want := []string{
+		write,
+		sleep,
+		write,
+		sleep,
+		write,
+		sleep,
+		write,
+	}
+
+	if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+		t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+	}
+
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
